6db0edb9ae241632b010caf0292a1a77f1761ae1
Cancey-Dejean <canceyd@gmail.com>
2025-09-22 11:22:41 -0500

    Fix button positioning by adding fixed class for theme toggle

--- src/App.svelte
+++ src/App.svelte
@@ -13,7 +13,7 @@
   <h1 class="text-3xl font-bold underline">
 Vite + Svelte</h1>
 
-<Button onclick={toggleMode} variant="outline" size="icon">
+<Button onclick={toggleMode} variant="outline" size="icon" class="fixed bottom-5 right-5">
   <SunIcon
     class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 !transition-all dark:-rotate-90 dark:scale-0"
   />
