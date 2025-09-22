c97ce6a89f107e847297c9175f081366732c50fd
Cancey-Dejean <canceyd@gmail.com>
2025-09-22 11:06:20 -0500

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import './app.css';
+  import svelteLogo from './assets/svelte.svg';
+  import viteLogo from '/vite.svg';
+  import Counter from './lib/Counter.svelte';
+</script>
+
+<main>
+  <h1 class="text-3xl font-bold underline">Vite + Svelte</h1>
+</main>
